27a2707b210667e1f6dd86e9dc76fc687045eb06
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-09-29 16:37:41 +0300

    first app from tutorial site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd087b929077c0a0a5ffe479e3a6a0ed4fc50b6764eeb18a7fb0c100dfd2e25f4974e9bbb2cef688ca83db50ea490d5f0a639235efb3989c67530e335d24cadc0'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+FirstApp::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  root :to => 'welcome#index'
+end
